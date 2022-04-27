fd3be418886b0ee1abfbc743a947261d223485cf
Rodrigo de Castro <rodrigo.cs.dev@gmail.com>
2022-04-26 22:21:29 -0300

    requisito 5

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const buttonValidation = () => {
+    const val = /\S+@\S+\.\S+/;
+    const validationEmail = val.test(email);
+    const seis = 6;
+    return !(validationEmail && password.length > seis);
+  };
+
   return (
     <main>
       <h2>Login</h2>
       <form>
-        <input data-testid="email-input" placeholder="Email" />
-        <input data-testid="password-input" placeholder="Password" />
-        <button data-testid="login-submit-btn" type="submit">
+        <input
+          data-testid="email-input"
+          placeholder="Email"
+          type="email"
+          value={ email }
+          onChange={ (e) => setEmail(e.target.value) }
+        />
+        <input
+          data-testid="password-input"
+          placeholder="Password"
+          type="password"
+          value={ password }
+          onChange={ (e) => setPassword(e.target.value) }
+        />
+        <button
+          data-testid="login-submit-btn"
+          type="submit"
+          disabled={ buttonValidation() }
+        >
           Enter
         </button>
       </form>
