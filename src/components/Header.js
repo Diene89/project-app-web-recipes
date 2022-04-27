47eb0bf20bd8061c0649960bb0c446162e9a30b0
rmjo <rodrigormjo@gmail.com>
2022-04-27 15:44:35 -0300

    requisito 11 Ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title, showSearchIcon }) {
   return (
     <header>
-      <img data-testid="profile-top-btn" src={ profileIcon } alt="profileIcou" />
+      <Link to="/profile">
+        <button type="button">
+          <img
+            data-testid="profile-top-btn"
+            src={ profileIcon }
+            alt="profileIcou"
+          />
+        </button>
+      </Link>
       <span data-testid="page-title">{title}</span>
       {
         showSearchIcon
@@ -22,7 +31,7 @@ Header.defaultProps = {
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
-  showSearchIcon: PropTypes.string,
+  showSearchIcon: PropTypes.bool,
 };
 
 export default Header;
