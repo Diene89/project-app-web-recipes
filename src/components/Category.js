0ddfaf8ccda45a081a98ca2795a475ca140e585d
Diene <diene_89@hotmail.com>
2022-04-29 17:45:33 -0300

    requisito 28 incompleto

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Category({ btnName }) {
+function Category({ btnName, btnClick }) {
   return (
-    <button type="button" data-testid={ `${btnName}-category-filter` }>{btnName}</button>
+    <button
+      type="button"
+      data-testid={ `${btnName}-category-filter` }
+      value={ btnName }
+      onClick={ (e) => btnClick(e.target.value) }
+    >
+      {btnName}
+    </button>
   );
 }
 
 Category.propTypes = {
   btnName: PropTypes.string.isRequired,
+  btnClick: PropTypes.func.isRequired,
 };
 
 export default Category;
--- src/pages/MainFood.js
+++ src/pages/MainFood.js
@@ -4,10 +4,11 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import RecipeCard from '../components/RecipeCard';
 import Category from '../components/Category';
-import { getFoodCategories, getFoods } from '../services/RecipesAPI';
+import { getFoodCategories, getFoods, getFoodsByCategory } from '../services/RecipesAPI';
 
 function MainFood() {
   const [categories, setCategories] = useState([]);
+  const [isFiltered, setisFiltered] = useState(false);
   const { recipes, initialRecipes } = useContext(AppContext);
 
   const getRecipes = async () => {
@@ -30,16 +31,28 @@ function MainFood() {
     }
   };
 
+  const getByCategory = async (category) => {
+    try {
+      const data = await getFoodsByCategory(category);
+      const recipesReceived = await data.meals;
+      initialRecipes(recipesReceived);
+      setisFiltered(!isFiltered);
+    } catch (error) {
+      initialRecipes(error);
+    }
+  };
+
   useEffect(() => { getRecipes(); getCategories(); }, []);
   const recipesQuantityLimit = 12;
   const categoriesQuantityLimit = 5;
   return (
     <main className="MainFood">
-      <Header title="Foods" />
+      <Header title="Foods" toRedirect={ isFiltered } />
       {categories
         .filter((category, index) => index < categoriesQuantityLimit)
         .map((category, index) => (<Category
           btnName={ category.strCategory }
+          btnClick={ getByCategory }
           key={ index }
         />))}
       {recipes
