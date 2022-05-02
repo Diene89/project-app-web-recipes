b9bfeeedc228e6216903928d05f33a9060936851
rmjo <rodrigormjo@gmail.com>
2022-05-02 18:06:42 -0300

    iniciando tela de explorar

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -6,6 +6,12 @@ function Explore() {
   return (
     <main>
       <Header title="Explore" showSearchIcon={ false } />
+      <button type="button" name="Explore Foods" data-testid="explore-foods">
+        Explore Foods
+      </button>
+      <button type="button" name="Explore Drinks" data-testid="explore-drinks">
+        Explore Drinks
+      </button>
       <Footer />
       Explore
     </main>
