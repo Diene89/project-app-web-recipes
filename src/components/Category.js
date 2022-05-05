c35286ecbac8728f39e3bf1742f75c739a018dda
Diene <diene_89@hotmail.com>
2022-05-05 14:28:56 -0300

    atualiza category

--- src/components/Category.js
+++ src/components/Category.js
@@ -4,15 +4,15 @@ import PropTypes from 'prop-types';
 function Category({ btnName, btnClick }) {
   return (
     <button
+      type="button"
+      data-testid={ `${btnName}-category-filter` }
+      value={ btnName }
+      onClick={ (e) => btnClick(e.target.value) }
       style={ { marginLeft: '10px',
         margin: '6px',
         textAlign: 'center',
         borderRadius: '10px',
         border: 'none' } }
-        type="button"
-        data-testid={ `${btnName}-category-filter` }
-        value={ btnName }
-        onClick={ (e) => btnClick(e.target.value) }
     >
       {btnName}
     </button>
