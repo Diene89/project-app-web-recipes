123dd16c9df97cf7962365a947a4d998a7b28a5f
Diene <diene_89@hotmail.com>
2022-05-05 18:02:49 -0300

    atualizando helper

--- src/helper.js
+++ src/helper.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
+import { render } from '@testing-library/react';
 
-const renderWithRouter = (route) => {
+export default function renderWithRouter(component) {
   const history = createMemoryHistory();
-  return ({
-    ...render(<Router history={ history }>{route}</Router>),
+  return {
+    ...render(
+      <Router history={ history }>
+        {component}
+      </Router>,
+    ),
     history,
-  });
-};
-
-export default renderWithRouter;
+  };
+}
