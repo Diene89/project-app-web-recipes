136e96212c276815cfa263fee874010c5dde6a32
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-05-05 14:06:36 -0300

    Testes da tela de explorar feitos

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
-// import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
 import AppProvider from './context/AppProvider';
 import Routes from './components/Routes';
 
 function App() {
   return (
     <AppProvider>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <Routes />
     </AppProvider>
   );
 }
