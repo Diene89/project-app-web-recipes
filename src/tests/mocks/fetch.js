3f8cae3d9927e74e58906ea921b4aba60abfdff4
Rodrigo de Castro <rodrigo.cs.dev@gmail.com>
2022-05-09 19:03:24 -0300

    testes de páginas de detalhes de drinks e comidas

--- src/tests/mocks/fetch.js
+++ src/tests/mocks/fetch.js
@@ -1,4 +1,5 @@
 import meal from './meal';
+import drink from './drink';
 import meals from './meals';
 import drinks from './drinks';
 import mealIngredients from './mealIngredients';
@@ -9,6 +10,7 @@ const fetch = (url) => Promise.resolve({
   ok: true,
   json: () => {
     const { idMeal } = meal.meals[0];
+    const { idDrink } = drinks.drinks[0];
     console.log('URL', url);
     switch (url) {
     case 'https://www.themealdb.com/api/json/v1/1/list.php?a=list':
@@ -16,6 +18,10 @@ const fetch = (url) => Promise.resolve({
     case 'https://www.themealdb.com/api/json/v1/1/random.php':
     case `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`:
       return Promise.resolve(meal);
+    case `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${idDrink}`:
+      return Promise.resolve(drinks.drinks);
+    case `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`:
+      return Promise.resolve(drink);
     case 'https://www.themealdb.com/api/json/v1/1/search.php?s=':
       return Promise.resolve(meals);
     case 'https://www.themealdb.com/api/json/v1/1/list.php?i=list':
@@ -29,3 +35,6 @@ const fetch = (url) => Promise.resolve({
 });
 
 export default fetch;
+
+/* `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`
+`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}` */
